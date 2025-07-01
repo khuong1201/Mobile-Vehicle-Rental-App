15258f599ceb3e50bd6f13790a25ca4423e1d4d9
khuong1201 <147017053+khuong1201@users.noreply.github.com>
2025-07-01 10:47:39 +0700

    mmomo payment

---
+++ frontend/lib/api_services/payment/momo_api.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/api_services/client/api_reponse.dart';
+import 'package:frontend/api_services/util/api_util.dart';
+import 'package:frontend/viewmodels/auth/auth_service.dart';
+
+class PaymentMomoApi {
+  static Future<ApiResponse<dynamic>>
+  createMomoPayment<T extends ChangeNotifier>({
+    required T viewModel,
+    required AuthService apiAuthService,
+    required Map<String, dynamic> paymentData,
+  }) async {
+    try {
+      final response = await callProtectedApi(
+        viewModel,
+        authService: apiAuthService,
+        endpoint: '/api/payments/create-momo-payment',
+        method: 'POST',
+        body: paymentData,
+      );
+
+      if (!response.success || response.data == null) {
+        return ApiResponse(
+          success: false,
+          message: response.message ?? 'Tạo thanh toán MoMo thất bại',
+        );
+      }
+
+      final data = response.data;
+      return ApiResponse(
+        success: true,
+        data: data,
+        message: 'Tạo thanh toán MoMo thành công',
+      );
+    } catch (e) {
+      return ApiResponse(
+        success: false,
+        message: 'Lỗi tạo thanh toán MoMo: $e',
+      );
+    }
+  }
+}
